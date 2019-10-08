1ad96e27d6cff80f204f93bfe0ae438dc279e96d
Egor Karpov <karpo040501@gmail.com>
2019-10-08 17:54:59 +0300

    Added comments to test_config

--- test_config.py
+++ test_config.py
@@ -1,4 +1,4 @@
-target = 'main.py'
-tests = 'tests.txt'
-time_limit = 1.0
-print_failed_tests = False
+target = 'main.py'            # файл с решением
+tests = 'tests.txt'           # файл с тестами
+time_limit = 1.0              # ограничение по времени в секундах
+print_failed_tests = False    # выводить ли непройденные тесты вместе с вердиктами
