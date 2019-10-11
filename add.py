6a039c77494bcc4ad22afb1f49d46d39a476a21e
Egor <karpo040501@gmail.com>
2019-10-11 13:05:34 +0300

    added tests adding

---
+++ add.py
@@ -0,0 +1,29 @@
+from test_config import tests
+# tests = 'test.txt'
+f = open(tests, 'a')
+
+while True:
+    print('Введите тест. Когда тест закончится, введите пустую строку.')
+    test = ''
+    s = input()
+    while s:
+        test += s + '\n'
+        s = input()
+
+    f.write(test)
+    f.write('\n')
+
+    ans = ''
+    print('Введите ответ. Когда ответ закончится, введите пустую строку.')
+    s = input()
+    while s:
+        ans += s + '\n'
+        s = input()
+
+    f.write(ans + '===\n')
+
+    cont = input('Вы хотите добавить еще? (y/n): ')
+    if cont.lower() == 'n':
+        break
+
+print('Спасибо, что пользуетесь тестилкой!')
